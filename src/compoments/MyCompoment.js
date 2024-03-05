bc8774ce48c0bcd757b01c3354aaa39beaa34b29
lamquocvinh <lamvinh940@gmail.com>
2024-03-05 18:48:30 +0700

    componemt

---
+++ src/compoments/MyCompoment.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class MyComponemt extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello this is Component by Dinhdev</h1>
+            </div>
+        )
+    }
+}
+export default MyComponemt
